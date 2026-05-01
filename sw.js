713925ec516b721d911a76dec2daa11020b184ee
SalmanDesaign-er <sekretariat.salmantauhid@gmail.com>
2026-05-01 15:55:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UBAH ANGKA INI SETIAP KALI BOS UPDATE index.html (Misal: v2, v3, v4)
-const CACHE_NAME = 'qurban-appanggota-v3'; 
+const CACHE_NAME = 'qurban-appanggota-v3.2'; 
 
 const urlsToCache = [
  './',
